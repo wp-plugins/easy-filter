b32fb34c7994cd8c31a0d7f02be6a7d1fef00b94
ronisaha <ronisaha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-07 03:27:22 +0000

    version no fixed
    git-svn-id: https://plugins.svn.wordpress.org/easy-filter/tags/1.1@622005 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ez-filter.php
+++ ez-filter.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Easy Filter
 Plugin URI: http://wp-plugins.xiidea.net/easy-filter
-Easy Filter Improve The Filter option of list pages(posts/pages/any custom post types)
-Version: 1.01
+Description: Easy Filter Improve The Filter option of list pages(posts/pages/any custom post types)
+Version: 1.1
 Author: Roni Saha
 Author URI: http://helpful-roni.com
 */
